f3f4943d4dd11ac2a70d97acaf913fba89546372
Sean Chen <sean820816@yahoo.com.tw>
2015-12-03 14:48:02 +0800

    Init FilesHelper ok

---
+++ Readme.md
@@ -0,0 +1,7 @@
+To-do
+
+Integrate all libs that are useful.
+
+Pods:
+SwiftyJSON / for json handling
+Alamofire / for networking
---
+++ Swift_Codebase/FileHelper.swift
@@ -0,0 +1,78 @@
+//
+//  FileHelper.swift
+//  Swift_Codebase
+//
+//  Created by Sean Chen on 2015/12/3.
+//  Copyright © 2015年 Rings. All rights reserved.
+//
+import UIKit
+import Foundation
+
+class FileHelper: NSObject {
+    
+    static let sharedInstance = FileHelper()
+    
+    func getDocumentsURL() -> NSURL {
+        let documentsURL = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0]
+        return documentsURL
+    }
+    
+    func getFilePathInDocumentsDirectory(filename: String) -> String {
+        let fileURL = getDocumentsURL().URLByAppendingPathComponent(filename)
+        return fileURL.path!
+    }
+    
+    /**
+    Get UIImage by path
+    
+    - parameter path: File path
+    
+    - returns: UIImage
+    */
+    func loadImageFromPath(path: String) -> UIImage? {
+        
+        let image = UIImage(contentsOfFile: path)
+        
+        if image == nil {
+            
+            print("missing image at: \(path)")
+        }
+        print("Loading image from path: \(path)") // this is just for you to see the path in case you want to go to the directory, using Finder.
+        return image
+        
+    }
+    
+    func listFilesInDocumentFolder() -> [String] {
+        let dirs = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true) as? [String]
+        if dirs != nil {
+            let dir = dirs![0]
+            do {
+                let fileList = try NSFileManager.defaultManager().contentsOfDirectoryAtPath(dir)
+                return fileList as [String]
+            }catch {
+                
+            }
+            
+        }else{
+            let fileList = [""]
+            return fileList
+        }
+        let fileList = [""]
+        return fileList
+    }
+    
+    func removeAllFilesInDocumentFolder() {
+        
+        let fileManager = NSFileManager.defaultManager()
+        
+        for file in listFilesInDocumentFolder() {
+            do {
+                try fileManager.removeItemAtPath(getFilePathInDocumentsDirectory(file))
+            } catch {
+                
+            }
+        }
+        
+        print("After cleanup document folder: \(listFilesInDocumentFolder())")
+    }
+}
