5963d2a784fc7b9b766f6652382da8ce5874459f
Sean Chen <sean820816@yahoo.com.tw>
2016-06-07 12:20:25 +0800

    KeyboardMaster basic ok

--- Swift_Codebase/KeyboardMaster.swift
+++ Swift_Codebase/KeyboardMaster.swift
@@ -72,7 +72,6 @@ class KeyboardMaster: NSObject {
     
     }
     
-    
     // Put in didEndEditing
     class func moveEverythingBackToOriginal(target target:UIViewController) {
         UIView.animateWithDuration(0.5, animations: { () -> Void in
@@ -86,5 +85,76 @@ class KeyboardMaster: NSObject {
             targetView.bounds.origin.y = 0
         })
     }
+    
+    //MARK: - COMPLETELY NEW WAY TO HANDLE KEYBOARD ISSUE
+    class func monitorViewsWhoAreCoveredByKeyboard(views:[UIView], targetView: UIView, yOffSet:CGFloat) {
+        // LISTEN TO WHEN KEYBOARD SHOWN
+        NSNotificationCenter.defaultCenter().addObserverForName(UIKeyboardWillShowNotification, object: nil, queue: NSOperationQueue.mainQueue()) { (notification) in
+            let kbTopY = KeyboardMaster.getKeyboardHeightFromNotificationDictionary(notification).keyboardY
+            
+            // FIND OUT WHO ARE FIRST RESPONDERS
+            for view in views {
+                if view.isFirstResponder() {
+                    let viewBottom = KeyboardMaster.getViewBottom(view, baseView: targetView)
+                    if viewBottom > kbTopY { // IS BELOW THE KEYBOARD
+                        print("\(view) is covered by keyboard!")
+                        
+                        // MOVE UP THE KEYBOARD
+                        KeyboardMaster.moveTheTargetView(targetView, direction: KeyboardMaster.MovingDirection.Up, yOffset: 0)
+                        
+                    } else { // NOT BELOW THE KEYBOARD
+                        
+                    }
+                }
+            }
+        }
+        
+        NSNotificationCenter.defaultCenter().addObserverForName(UIKeyboardWillHideNotification, object: nil, queue: NSOperationQueue.mainQueue()) { (notification) in
+            
+            KeyboardMaster.moveTheTargetView(targetView, direction: KeyboardMaster.MovingDirection.Down, yOffset: 0)
+            
+        }
+    }
+    
+    enum MovingDirection {
+        case Up
+        case Down
+    }
+    
+    // MOVE THE AVOID KEYBOARD
+    private class func moveTheTargetView(targetView: UIView, direction: KeyboardMaster.MovingDirection, yOffset: CGFloat) {
+        UIView.animateWithDuration(0.5, animations: {
+            if direction == .Up {
+                let distanceWithDirection: CGFloat = CGFloat(50)
+                targetView.bounds.origin.y = targetView.bounds.origin.y + distanceWithDirection + yOffset
+            } else if direction == .Down {
+                targetView.bounds.origin.y = 0
+            }
+            
+            }) { (done) in
+        }
+    }
+    
+    private class func getViewBottom(view: UIView, baseView: UIView) -> CGFloat {
+        return KeyboardMaster.getViewFrameFromInBaseView(view, baseView: baseView).origin.y + view.frame.size.height
+    }
+    
+    // GET VIEW'S FRAME THAT IS CONVERTED BY self.view
+    // TODO: NESTED VIEW SITUATION
+    private class func getViewFrameFromInBaseView(view: UIView, baseView: UIView) -> CGRect {
+        let frame = baseView.convertRect(view.frame, fromView: view.superview!)
+        return frame
+    }
+    
+    private class func getKeyboardHeightFromNotificationDictionary(notification: NSNotification) -> (keyboardHeight: CGFloat, keyboardY: CGFloat) {
+        var dict:Dictionary = notification.userInfo!
+        let kbValue:NSValue = dict[UIKeyboardFrameEndUserInfoKey]! as! NSValue
+        let kbRect:CGRect = kbValue.CGRectValue()
+        let keyboardHeight: CGFloat = kbRect.height
+        let fromTopToKeyboard: CGFloat = UIScreen.mainScreen().bounds.height - keyboardHeight
+        return (keyboardHeight, fromTopToKeyboard)
+    }
+    
+
 
 }
