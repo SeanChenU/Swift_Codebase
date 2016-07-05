35439efe3910a80c3ba602fd4abc211afa89fb58
Sean Chen <sean820816@yahoo.com.tw>
2016-07-05 14:36:53 +0800

    Owner signup info flow ok

---
+++ Swift_Codebase/ReminderText.swift
@@ -0,0 +1,19 @@
+//
+//  ReminderText.swift
+//  vetcare-ios
+//
+//  Created by SEAN HD on 2016/7/5.
+//  Copyright © 2016年 IgnioLab. All rights reserved.
+//
+
+import UIKit
+
+//class ReminderText: NSObject {
+//
+//}
+
+enum ReminderText: String {
+    case FillAllTextField = "請輸入所有欄位 🙏🏼"
+    case ReadUserTerm = "請確認使用者條款"
+    case ChooseImage = "請選擇您的大頭照"
+}
