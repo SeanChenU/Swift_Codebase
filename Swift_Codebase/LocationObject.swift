41263c96ec7b70c92c3045d387d8251683458e83
Sean Chen <sean820816@yahoo.com.tw>
2016-07-19 23:01:11 +0800

    Added locationObject

---
+++ Swift_Codebase/LocationObject.swift
@@ -0,0 +1,31 @@
+//
+//  LocationObject.swift
+//  vetcare-ios
+//
+//  Created by SEAN HD on 2016/7/19.
+//  Copyright © 2016年 IgnioLab. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class LocationObject: NSObject {
+    var coordinate: CLLocationCoordinate2D
+    var address: String
+    
+    required init(coordinate: CLLocationCoordinate2D, address: String) {
+        self.coordinate = coordinate
+        self.address = address
+    }
+    
+    internal class func objectFromJSON(locationJSON: JSON) -> LocationObject {
+        let loc = locationJSON["loc"]
+        let coordinate = CLLocationCoordinate2D(latitude: loc["lat"].numberValue.doubleValue, longitude: loc["lng"].numberValue.doubleValue)
+        let address = locationJSON["text"].stringValue
+        return LocationObject(coordinate: coordinate, address: address)
+    }
+    
+    override var description: String {
+        return "<LocationObject> lat: \(self.coordinate.latitude), lng: \(self.coordinate.longitude), address: \(self.address)"
+    }
+}
